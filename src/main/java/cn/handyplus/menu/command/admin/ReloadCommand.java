f117254bd2a0ad665d0b353b0589eccaba14bbc2
handy <hanydplus@163.com>
2024-12-21 09:40:39 +0800

    1.4.4 重载插件的时候关闭全部当前打开的菜单

--- src/main/java/cn/handyplus/menu/command/admin/ReloadCommand.java
+++ src/main/java/cn/handyplus/menu/command/admin/ReloadCommand.java
@@ -1,6 +1,7 @@
 package cn.handyplus.menu.command.admin;
 
 import cn.handyplus.lib.command.IHandyCommandEvent;
+import cn.handyplus.lib.inventory.HandyInventoryUtil;
 import cn.handyplus.lib.util.BaseUtil;
 import cn.handyplus.lib.util.MessageUtil;
 import cn.handyplus.menu.util.ConfigUtil;
@@ -25,13 +26,11 @@ public class ReloadCommand implements IHandyCommandEvent {
     }
 
     @Override
-    public boolean isAsync() {
-        return true;
-    }
-
-    @Override
     public void onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        // 重载配置
         ConfigUtil.init();
+        // 同步关闭当前打开的gui
+        HandyInventoryUtil.closeHandyInventory();
         MessageUtil.sendMessage(sender, BaseUtil.getMsgNotColor("reloadMsg"));
     }
 
